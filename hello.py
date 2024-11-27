eb97a01d373fe9cb550590d00245c56eafeefd12
iiikey <yui@macstudent.com.au>
2024-11-27 12:06:24 +1100

    Watched up to 53 mins in video

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
+#Ask user for their name
 name = input("What's your name? ")
-print("Hello, " + name)
+
+#Removes white space from str and capitalises user's name
+name = name.strip( ).title( )
+
+#Say hello to user
+print(f"Hello, {name}")
