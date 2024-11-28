700921daa28e6851195d59a5397076d964db7ce1
iiikey <yui@macstudent.com.au>
2024-11-29 08:40:31 +1100

    finished playback.py

---
+++ playback.py
@@ -0,0 +1,8 @@
+# Get user input
+userinput = input("Enter an input: ")
+
+# Replace spaces with something else (e.g., a dash)
+replaced = userinput.replace(" ", "...")
+
+# Display the result
+print("Modified input:", replaced)
